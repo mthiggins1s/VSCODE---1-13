419a91d827fd9e09e0b20ba826585b4efb5c6a72
Matthew Higgins <mthiggins1s@semo.edu>
2025-01-13 18:49:34 -0600

    updated with html and js with working buttons.

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('text-one').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=NccwPo4NWVA';
+});
+
+document.getElementById('text-two').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=VNu15Qqomt8';
+});
+
+document.getElementById('text-three').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=cdKop6aixVE';
+});
+
+document.getElementById('text-four').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=XmjnMhMiyME';
+});
+
+document.getElementById('text-five').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=Z8FBnz6USIM';
+});
